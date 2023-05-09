b97058cfc2de7144d48ed31d20afeeb07201c3d0
Lukáš Hajnyš <hajnysl22@spstrutnov.cz>
2023-05-09 08:16:03 +0200

    Obrázek se už doufám načítá

--- src/obj/tablet/tablet_button.py
+++ src/obj/tablet/tablet_button.py
@@ -4,7 +4,7 @@ from obj.element import Element
 class TabletButton:
     
     def __init__(self):
-        self.image = pygame.image.load("obj/tablet/Visuals/tabletbutton.png")
+        self.image = pygame.image.load("obj/tablet/tabletbutton.png")
         self.rect = self.image.get_rect()
 
     def update(self):
