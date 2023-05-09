d5de8f0af38143c5dbad9f5ff65a44ddf5568fff
Tomáš Krejdl <krejdlt22@spstrutnov.cz>
2023-05-09 09:23:35 +0200

    držení dveří

--- src/projekt 1ep.py
+++ src/projekt 1ep.py
@@ -1,6 +1,6 @@
 import pygame
 pygame.init()
-X,Y = 800,600
+X,Y = 1920,1080
 x, y, w, h = 200, 300, 50, 50
 r, g, b = 0, 255, 0
 okno = pygame.display.set_mode((X, Y))
@@ -10,11 +10,16 @@ while True:
             pygame.quit()
     
     mouse = pygame.mouse.get_pos()
+    mp = pygame.mouse.get_pressed(num_buttons=3)
     
-    if mouse[0] >= x and mouse[0] <= x+w and mouse[1] >= y and mouse[1] <= y+h:
+    if mouse[0] >= x and mouse[0] <= x+w and mouse[1] >= y and mouse[1] <= y+h and mp[0]:
         r,g,b = 255,0,0
     else:
         r,g,b = 0,255,0
+    
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        print(event.button)
+    
     okno.fill((255,255,255))
     pygame.draw.rect(okno, (r, g, b), (x, y, w, h))
     pygame.display.update()
