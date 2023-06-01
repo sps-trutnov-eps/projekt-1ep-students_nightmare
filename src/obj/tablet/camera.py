100e2783402f94076f1bcb4611ac5ab1063e6097
Tomáš Klose <kloset22@spstrutnov.cz>
2023-06-01 13:02:47 +0200

    Funkční zobrazování kamer na tabletu

--- src/obj/tablet/camera.py
+++ src/obj/tablet/camera.py
@@ -5,16 +5,12 @@ from obj.element import Element
 
 class Camera(Element):
 
-    def __init__(self, position: tuple[int, int], window: pygame.Surface, name: str, image: pygame.Surface):
+    def __init__(self, position: tuple[int, int], parent_surface: pygame.Surface, name: str, image: pygame.Surface = None):
+        if image != None:
+            self.image = image
         self.name = name
-        self.image = image
-        image_rect = self.image.get_rect()
-        super().__init__(image_rect, window)
+        super().__init__((55,55), position, parent_surface)
 
         # Set position of image (otherwise it's (0, 0))
         self.x = position[0]
         self.y = position[1]
-        
-       
-    def update(self):
-        self.window.blit(self.image, (self.x, self.y))
--- src/obj/tablet/camera_list.py
+++ src/obj/tablet/camera_list.py
@@ -6,20 +6,18 @@ from obj.tablet.camera import Camera
 
 class CameraList(Element):
 
-    def __init__(self, tablet, window: pygame.Surface) -> None:
-        rect = pygame.Rect(tablet.x, tablet.y, 300, 40)
-        super().__init__(rect, window)
+    def __init__(self, tablet, parent_surface: pygame.Surface) -> None:
+        super().__init__((400, 80), (20, 20), parent_surface)
         self.camera_count = 5
         self.list: list[Camera] = []
         self.surface = pygame.Surface((self.width, self.height))
+        self.surface.fill((255, 255, 255))
         
         i = 0
-        while i > self.camera_count:
-            image = pygame.image.load('visuals/levedverezavrene.png')
-            camera = Camera((tablet.x, tablet.y), self.window, "Name", image)
-            print(camera.image)
+        while i < self.camera_count:
+            image = pygame.Surface((55, 55))
+            image.fill((255,0,0))
+            camera = Camera((tablet.x, tablet.y), self.surface, "Name", image)
             self.list.append(camera)
+            self.surface.blit(camera.image, (40*i + 40*i, 0))
             i += 1
-
-        for camera in self.list:
-            self.surface.blit(camera.image)
