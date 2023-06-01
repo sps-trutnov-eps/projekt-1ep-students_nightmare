a2ca7be565176e902eef95fbe00b04816e275524
Jakub Tryzna <tryznaj22@spstrutnov.cz>
2023-06-01 13:18:30 +0200

    Už se to zapne
    amen

--- src/obj/doors/doors.py
+++ src/obj/doors/doors.py
@@ -3,58 +3,57 @@ import pygame
 import time
 ucitel_u_dveri = False
 
-okno = pygame.display.set_mode((X, Y))
 
-class dvere:
-    def __init__(self,mys, tlacitko_rozmery, tlacitko_barva):
-        self.mys_vodorovne = list(mys[0])
-        self.mys_svisle = list(mys[1])
-        self.tlacitko_x = list(tlacitko[0])
-        self.tlacitko_y = list(tlacitko[1])
-        self.tlacitko_w = list(tlacitko[2])
-        self.tlacitko_h = list(tlacitko[3])
-        self.tlacitko_r = list(tlacitko_barva[0])
-        self.tlacitko_g = list(tlacitko_barva[0])
-        self.tlacitko_b = list(tlacitko_barva[0])
+
+class Dvere:
+    def __init__(self, mys, tlacitko_rozmery, tlacitko_barva, dvere_rozmery):
+        self.mys_vodorovne = mys[0]
+        self.mys_svisle = mys[1]
+        self.tlacitko_rozmery = list(tlacitko_rozmery)
+        self.tlacitko_barva = list(tlacitko_barva)
+        self.dvere_rozmery = list(dvere_rozmery)
+        self.dvere_barva = 0,255,0
         
         
         
     def ucitel_u_dveri(self):
-        ucitel_u_dveri = False 
+        self.ucitel_u_dveri = False 
         if random.randint(0, 1000) < 2 and not ucitel_u_dveri:
-            ucitel_u_dveri = True
+            self.ucitel_u_dveri = True
             self.cas_ucitel = pygame.time.get_ticks() / 1000
             self.cas_jumpscare = 6 
-        else:
-            pass
+
         
         
     def get_pressed(self):
+        self.get_pressed = False 
         mys_na_tlacitku = mys_vodorovne and mys_svisle
         mp = pygame.mouse.get_pressed(num_buttons=3)
         mys_stisk = mp[0]
         tlacitko_stisknute = mys_na_tlacitku and mys_stisk
         # tlacitko
         if tlacitko_stisknute:
-            tlacitko_r,tlacitko_g,tlacitko_b = 255,0,0
+            self.get_pressed = True 
         else:
-            tlacitko_r,tlacitko_g,tlacitko_b = 0,255,0
-
+            self.tlacitko_barva = 0,255,0
+            
+            
+    def jumpscare(self):
         # dvere
-        if not tlacitko_stisknute and not ucitel_u_dveri:
-            dvere_r,dvere_g,dvere_b = 0,255,0
-        elif not tlacitko_stisknute and ucitel_u_dveri:
-            dvere_r,dvere_g,dvere_b = 0,0,255
-        elif tlacitko_stisknute:
-            dvere_r,dvere_g,dvere_b = 255,0,0
-        if ucitel_u_dveri:
-            cas_ted = pygame.time.get_ticks() / 1000
-            if cas_ted - cas_ucitel >= cas_jumpscare:
+        if self.get_pressed == False and self.ucitel_u_dveri:
+            self.dvere_barva = 0,0,255
+            self.cas_ted = pygame.time.get_ticks() / 1000
+            if self.cas_ted - self.cas_ucitel >= self.cas_jumpscare:
                 print("fatty")
-        if ucitel_u_dveri and tlacitko_stisknute:
-            ucitel_u_dveri = False 
-        
-        okno.fill((255,255,255))
-        pygame.draw.rect(okno, (dvere_r,dvere_g,dvere_b), (x, y, w, h))
-        pygame.draw.rect(okno, (tlacitko_r, tlacitko_g, tlacitko_b), (tlacitko_x,tlacitko_y,tlacitko_w,tlacitko_h))
-        pygame.display.update()
+        elif self.ucitel_u_dveri and self.get_pressed:
+            ucitel_u_dveri = False
+            self.tlacitko_barva = 0,0,255
+            self.dvere_barva = 0,0,255
+        else:
+            self.tlacitko_barva = 0,255,0
+            self.dvere_barva = 0,255,0
+            
+    def render(self, window):
+        pygame.draw.rect(window, self.dvere_barva, self.dvere_rozmery)
+        pygame.draw.rect(window, self.tlacitko_barva, self.tlacitko_rozmery)
+         
--- src/projekt 1ep.py
+++ src/projekt 1ep.py
@@ -1,60 +1,19 @@
 import random
 import pygame
 import time
+from obj.doors.doors import Dvere
 pygame.init()
-X,Y = 800,600
-x, y, w, h = 400, 300, 50, 50
-tlacitko_x, tlacitko_y, tlacitko_w, tlacitko_h = 300,300 ,50 ,50
-tlacitko_r, tlacitko_g, tlacitko_b = 0,255,0
-dvere_r,dvere_g,dvere_b = 0,255,0
-
-ucitel_u_dveri = False
-
-okno = pygame.display.set_mode((X, Y))
+mys = pygame.mouse.get_pos()
+okno = pygame.display.set_mode((800,800))
+doors = Dvere(mys, (400,300,50,50), (0,255,0), (400,300,50,50))
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            
     
-    if random.randint(0, 1000) < 2 and not ucitel_u_dveri:
-        ucitel_u_dveri = True
-        cas_ucitel = pygame.time.get_ticks() / 1000
-        cas_jumpscare = 6
-        print(cas_ucitel)
-  
-        
-    else:
-        pass
     
-    mouse = pygame.mouse.get_pos()
-    mp = pygame.mouse.get_pressed(num_buttons=3)
-    
-    mys_vodorovne = mouse[0] >= tlacitko_x and mouse[0] <= tlacitko_x+tlacitko_w
-    mys_svisle = mouse[1] >= tlacitko_y and mouse[1] <= tlacitko_y+tlacitko_h
-    mys_na_tlacitku = mys_vodorovne and mys_svisle
-    mys_stisk = mp[0]
-    tlacitko_stisknute = mys_na_tlacitku and mys_stisk
-    # tlacitko
-    if tlacitko_stisknute:
-        tlacitko_r,tlacitko_g,tlacitko_b = 255,0,0
-    else:
-        tlacitko_r,tlacitko_g,tlacitko_b = 0,255,0
 
-    # dvere
-    if not tlacitko_stisknute and not ucitel_u_dveri:
-        dvere_r,dvere_g,dvere_b = 0,255,0
-    elif not tlacitko_stisknute and ucitel_u_dveri:
-        dvere_r,dvere_g,dvere_b = 0,0,255
-    elif tlacitko_stisknute:
-        dvere_r,dvere_g,dvere_b = 255,0,0
-    if ucitel_u_dveri:
-        cas_ted = pygame.time.get_ticks() / 1000
-        if cas_ted - cas_ucitel >= cas_jumpscare:
-            print("fatty")
-    if ucitel_u_dveri and tlacitko_stisknute:
-        ucitel_u_dveri = False 
-    
     okno.fill((255,255,255))
-    pygame.draw.rect(okno, (dvere_r,dvere_g,dvere_b), (x, y, w, h))
-    pygame.draw.rect(okno, (tlacitko_r, tlacitko_g, tlacitko_b), (tlacitko_x,tlacitko_y,tlacitko_w,tlacitko_h))
+    doors.render(okno)
     pygame.display.update()
